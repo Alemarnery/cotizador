34914cd579b46b555a734773a1ff9088ad289086
Alemarnery <aleemar.95@gmail.com>
2021-09-08 22:21:11 -0400

    resultado

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styled from "@emotion/styled";
+import { calcularMarca, obtenerDiferenciaYear } from "../helper";
 
 const Campo = styled.div`
   display: flex;
@@ -20,7 +21,7 @@ const Select = styled.select`
 `;
 
 const InputRadio = styled.input`
-  margin: 0 1 rem;
+  margin: 0 1rem;
 `;
 
 const Button = styled.button`
@@ -40,22 +41,51 @@ const Button = styled.button`
   }
 `;
 
+const Error = styled.div`
+  background-color: red;
+  color: white;
+  padding: 1rem;
+  width: 100%;
+  text-align: center;
+  margin-bottom: 2rem;
+`;
+
 const Formulario = () => {
   const [datos, guardarDatos] = useState({
     marca: "",
-    year: "",
+    ano: "",
     plan: "",
   });
 
+  const [error, setError] = useState(false);
+
   //extraer los valores del state
-  const { marca, year, plan } = datos;
+  const { marca, ano, plan } = datos;
 
   const saveData = (e) => {
     guardarDatos({ ...datos, [e.target.name]: e.target.value });
   };
 
+  const cotizarSeguro = (e) => {
+    e.preventDefault();
+    if (marca.trim() === "" || ano.trim() === "" || plan.trim() === "") {
+      setError(true);
+      return;
+    }
+    setError(false);
+
+    let resultado = 2000;
+
+    const diferencia = obtenerDiferenciaYear(ano);
+    resultado -= (diferencia * 3 * resultado) / 100;
+
+    resultado = calcularMarca(marca) * resultado;
+  };
+
   return (
-    <form>
+    <form onSubmit={cotizarSeguro}>
+      {error ? <Error>todos los campos son obligatorios</Error> : null}
+
       <Campo>
         <Label>Marca</Label>
         <Select name="marca" value={marca} onChange={saveData}>
@@ -67,7 +97,7 @@ const Formulario = () => {
       </Campo>
       <Campo>
         <Label>Ano</Label>
-        <Select name="ano" value={year} onChange={saveData}>
+        <Select name="ano" value={ano} onChange={saveData}>
           <option value="">-- Seleccione --</option>
           <option value="2021">2021</option>
           <option value="2020">2020</option>
@@ -88,20 +118,20 @@ const Formulario = () => {
           name="plan"
           value="basico"
           checked={plan === "basico"}
-          onChange={saveData(e)}
+          onChange={saveData}
         />
         Basico
         <InputRadio
           type="radio"
           name="plan"
-          value="Completo"
+          value="completo"
           checked={plan === "completo"}
           onChange={saveData}
         />
         Completo
       </Campo>
 
-      <Button type="button">Cotizar</Button>
+      <Button type="submit">Cotizar</Button>
     </form>
   );
 };
